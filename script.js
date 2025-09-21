9109fb3feb32925f8ceab9b58b75ac9f1a63c15c
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2025-09-21 20:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const audioElements = {};
+
+sounds.forEach(sound => {
+    audioElements[sound] = new Audio(`sounds/${sound}.mp3`);
+});
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        stopAll();
+        audioElements[sounds[idx]].currentTime = 0;
+        audioElements[sounds[idx]].play();
+    });
+});
+
+function stopAll() {
+    Object.values(audioElements).forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
+
+document.querySelector('.stop').addEventListener('click', stopAll);
